9fc24e0506cfe8f64a8255c68f43f920b0955669
Colden Cullen <ColdenCullen@gmail.com>
2013-12-10 16:16:47 -0500

    Switched to ClassInfo alias, made getters pure, explained turtles.

--- Axle/backend/atype.d
+++ Axle/backend/atype.d
@@ -4,7 +4,7 @@ import backend.aobject;
 template Property( string type, string name, string setterAccess = "private", string checkExpr = "true" ) {
 	const char[] Property = 
 		"private " ~ type ~ " _" ~ name ~ ";\n" ~
-		"public @property " ~ type ~ " " ~ name ~ "() { return _" ~ name ~ "; }\n" ~
+		"public @property " ~ type ~ " " ~ name ~ "() { return _" ~ name ~ "; } pure\n" ~
 		setterAccess ~ " @property void " ~ name ~ "( " ~ type ~ " val ) { if( " ~ checkExpr ~ " ) _" ~ name ~ " = val; }\n";
 }
 
@@ -14,13 +14,17 @@ public:
 	mixin( Property!( "string", "name" ) );
 	mixin( Property!( "aType", "base" ) );
 
-	this( TypeInfo_Class type )
+	this( ClassInfo type )
 	{
 		this.name = type.name;
 
 		if( type.base !is null )
 		{
 			this.base = new aType( type.base );
+		}
+		else
+		{
+			// Base is turtles
 		}
 	}
 }
